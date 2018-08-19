aa27b226967a74110fafe708d568b832e16a04b8
steine18 <michaelsteiner22@mac.com>
2018-08-19 10:10:51 -0700

    Added views to quote admin

--- wedding/admin.py
+++ wedding/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Hotel, Fund, FunStuff, Local, Quote
 
+
+class QuoteAdmin(admin.ModelAdmin):
+    search_fields = ['place', 'user']
+
+    list_display = ('place', 'user', 'approved')
+
+
 # Register your models here.
 admin.site.register(Hotel)
 admin.site.register(Fund)
 admin.site.register(FunStuff)
 admin.site.register(Local)
-admin.site.register(Quote)
+admin.site.register(Quote, QuoteAdmin)
+
